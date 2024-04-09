05e39e10de44474d7015390f97a198550ecaebab
Tolic031 <ivan.tolic1989@gmail.com>
2024-04-09 08:30:43 +0200

    napravljen entitet mec te stavljen u context

--- Backend/Data/TeniskaLigaContext.cs
+++ Backend/Data/TeniskaLigaContext.cs
@@ -13,5 +13,19 @@ namespace Backend.Data
         public DbSet<Natjecatelj>Natjecatelji { get; set; }
         public DbSet<Sezona> Sezone { get; set; }
 
+        public DbSet<Mec>Mecevi { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Mec>().HasOne(m => m.Izazivac);
+            modelBuilder.Entity<Mec>().HasOne(m => m.Izazvani);
+            modelBuilder.Entity<Mec>().HasOne(m => m.Sezona);
+            modelBuilder.Entity<Mec>().HasOne(m => m.Natjecatelj);
+        }
+
+
+
     }
 }
--- Backend/Models/Mec.cs
+++ Backend/Models/Mec.cs
@@ -1,6 +1,25 @@
-﻿namespace Backend.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
 {
-    public class Mec
+    public class Mec:Entitet
     {
+        [ForeignKey("izazivac")]
+        public required Izazivac Izazivac { get; set; }
+
+        [ForeignKey("izazvani")]
+        public required Izazvani Izazvani { get; set; }
+        public string? Pobjednik { get; set; }
+        public DateTime? Datum { get; set; }
+        public int? Red { get; set; }
+        public string? Napomena { get; set; }
+
+        [ForeignKey("sezona")]
+        public Sezona? Sezona { get; set; }
+
+        [ForeignKey("natjecatelj")]
+        public Natjecatelj? Natjecatelj { get; set; }
+
+
     }
 }
